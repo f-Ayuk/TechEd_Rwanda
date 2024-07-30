7c155fe8a2950c7b856ad437d165149bf1dc30ad
Favor Ayuk <127386070+f-Ayuk@users.noreply.github.com>
2024-07-30 21:01:57 +0200

    Delete TechEd_Rwandabackend directory

--- TechEd_Rwandabackend/routes/contentRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getCourses, getResources } = require('../controllers/contentController');
-
-router.get('/courses', getCourses);
-router.get('/resources', getResources);
-
-module.exports = router;
-
