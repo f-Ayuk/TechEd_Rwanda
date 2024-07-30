581f7226f12673e81bf50838d2696c107458526a
samkit4q <a.aderoba@alustudent.com>
2024-07-29 20:00:19 -0700

    script updated

--- TechEd_Rwandabackend/index.js
+++ TechEd_Rwandabackend/index.js
@@ -1,26 +1,20 @@
-// index.js
 const express = require('express');
 const mongoose = require('mongoose');
-const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const MONGO_URI = process.env.MONGO_URI;
 
-// Middleware
-app.use(express.json());
-app.use(cors());
+mongoose.connect(MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => {
+  console.log('MongoDB connected');
+}).catch(err => {
+  console.error('MongoDB connection error:', err.message);
+});
 
-// Routes
-const userRoutes = require('./routes/user');
-app.use('/api/users', userRoutes);
-
-// Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('MongoDB connected'))
-  .catch(err => console.log('MongoDB connection error:', err));
-
-// Start server
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
