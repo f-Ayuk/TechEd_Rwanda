b522957bfbe1880f89135df8ec9551119039e2e6
Daniel IRYIVUZE <139581457+Daniel-IRYIVUZE@users.noreply.github.com>
2024-07-31 21:10:36 +0200

    Update login.vue

--- frontend/src/views/login.vue
+++ frontend/src/views/login.vue
@@ -13,7 +13,7 @@ const router = useRouter(); // Initialize router
 const handleSubmit = async (e) => {
   e.preventDefault();
 
-  const response = await fetch("http://127.0.0.1:8000/login", {
+  const response = await fetch("https://teched-rwanda.onrender.com/login", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
